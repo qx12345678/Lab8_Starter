453f7f4ffc0291352153071c6f38a47b491c1ea9
Estella Li <liqx9979@gmail.com>
2022-12-07 06:22:47 -0800

    update unit test

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,84 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('matches valid phone numbers', ()=>{
+    expect(functions.isPhoneNumber('(619)-617-2001')).toBe(true);
+})
+
+test('matches valid phone numbers', ()=>{
+    expect(functions.isPhoneNumber('(617) 617-2001')).toBe(true);
+})
+
+test('matches valid phone numbers', ()=>{
+    expect(functions.isPhoneNumber('6161720')).toBe(false);
+})
+
+test('matches valid phone numbers', ()=>{
+    expect(functions.isPhoneNumber('6199 617 2001')).toBe(false);
+})
+
+test('matches valid emails', ()=>{
+    expect(functions.isPhoneNumber('abc@gmail.com')).toBe(true);
+})
+
+test('matches valid emails', ()=>{
+    expect(functions.isPhoneNumber('abc@email.com')).toBe(true);
+})
+
+test('matches valid emails', ()=>{
+    expect(functions.isPhoneNumber('1234')).toBe(false);
+})
+
+test('matches valid emails', ()=>{
+    expect(functions.isPhoneNumber('abcd')).toBe(false);
+})
+
+test('isStrongPassword', ()=>{
+    expect(functions.isStrongPassword('ab3466567')).toBe(true);
+})
+
+test('isStrongPassword', ()=>{
+    expect(functions.isStrongPassword('bbcdefg')).toBe(true);
+})
+
+test('isStrongPassword', ()=>{
+    expect(functions.isStrongPassword('12345678')).toBe(false);
+})
+
+test('isStrongPassword', ()=>{
+    expect(functions.isStrongPassword('a1')).toBe(false);
+})
+
+test('isDate', ()=>{
+    expect(functions.isDate('Jun/1st/2001')).toBe(false);
+})
+
+test('isDate', ()=>{
+    expect(functions.isDate('01/06/23')).toBe(false);
+})
+
+test('isDate', ()=>{
+    expect(functions.isDate('1/2/2021')).toBe(true);
+})
+
+test('isDate', ()=>{
+    expect(functions.isDate('06/21/2002')).toBe(true);
+})
+
+test('isHexColor', ()=>{
+    expect(functions.isHexColor('#ff0000')).toBe(true);
+})
+
+test('isHexColor', ()=>{
+    expect(functions.isDate('#dbd')).toBe(true);
+})
+
+test('isHexColor', ()=>{
+    expect(functions.isDate('1234')).toBe(false);
+})
+
+test('isHexColor', ()=>{
+    expect(functions.isDate('#33')).toBe(false);
+})
+
+
